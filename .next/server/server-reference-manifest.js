3a84380fa87be8ac96e02c48efc9067f4cb3fe20
squach90 <160175686+squach90@users.noreply.github.com>
2024-06-01 18:48:22 +0200

    fix4

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GIxBBBwjbW4oII1pdbq6n4KoVPk26AvbwxPLSLlQkcw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"8HAn2cEdNckiMDOiDL5NO3Tho8jNTmt9qHKpvK+qePw=\"}"
